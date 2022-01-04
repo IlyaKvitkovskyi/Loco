f3b2c3eda6c19b62f1201745447130c08492df36
Illia <ilya.kvitkovskyi@gmail.com>
2022-01-04 10:26:34 +0200

    added main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function() {
-
+$(document).ready(function(){
+  $('.content_toggle').click(function(){
+    $('.content_block').toggleClass('hide');	
+    if ($('.content_block').hasClass('hide')) {
+      $('.content_toggle').html('Показать всё');
+    } else {
+      $('.content_toggle').html('Скрыть');
+    }		
+    return false;
+  });				
 });
