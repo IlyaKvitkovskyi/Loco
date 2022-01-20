56a1b6bbfe555678b07873199ff8ab39c2ca6303
Illia <ilya.kvitkovskyi@gmail.com>
2022-01-20 13:48:44 +0200

    added fix to faq page

--- app/js/main.js
+++ app/js/main.js
@@ -7,63 +7,63 @@ $(function () {
 
     $this.toggleClass('active');
   });
-});
 
-var clickMe = document.querySelector('.content_toggle');
-var el = document.querySelectorAll('.content_block');
+  var clickMe = document.querySelector('.content_toggle');
+  var el = document.querySelectorAll('.content_block');
 
-document.addEventListener('click', function (event) {
-  if (!event.target.matches('.content_toggle')) return;
-  console.log(el);
-  el.forEach((item) => {
-    item.classList.toggle('hide');
+  document.addEventListener('click', function (event) {
+    if (!event.target.matches('.content_toggle')) return;
+    console.log(el);
+    el.forEach((item) => {
+      item.classList.toggle('hide');
+    });
+
+    if (el[0].classList.contains('hide')) {
+      clickMe.innerHTML = 'Показать всё';
+    } else {
+      clickMe.innerHTML = 'Скрыть';
+    }
+    return false;
   });
 
-  if (el[0].classList.contains('hide')) {
-    clickMe.innerHTML = 'Показать всё';
-  } else {
-    clickMe.innerHTML = 'Скрыть';
-  }
-  return false;
-});
+  new Swiper('.swiper1', {
+    // Optional parameters
+    width: 1485,
+    loop: false,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    // centeredSlides: true,
+    initialSlide: 3,
+    slideToClickedSlide: true,
+  });
 
-new Swiper('.swiper1', {
-  // Optional parameters
-  width: 1485,
-  loop: false,
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  // centeredSlides: true,
-  initialSlide: 3,
-  slideToClickedSlide: true,
-});
+  new Swiper('.swiper2', {
+    // Optional parameters
+    width: 1400,
+    loop: false,
+    slidesPerView: 'auto',
+    // spaceBetween: 30,
+    // centeredSlides: true,
+    initialSlide: 1,
+    slideToClickedSlide: true,
+  });
 
-new Swiper('.swiper2', {
-  // Optional parameters
-  width: 1400,
-  loop: false,
-  slidesPerView: 'auto',
-  // spaceBetween: 30,
-  // centeredSlides: true,
-  initialSlide: 1,
-  slideToClickedSlide: true,
-});
+  new Swiper('.swiper3', {
+    // Optional parameters
+    width: 1400,
+    loop: false,
+    slidesPerView: 'auto',
+    // spaceBetween: 30,
+    // centeredSlides: true,
+    initialSlide: 0,
+    slideToClickedSlide: true,
+  });
 
-new Swiper('.swiper3', {
-  // Optional parameters
-  width: 1400,
-  loop: false,
-  slidesPerView: 'auto',
-  // spaceBetween: 30,
-  // centeredSlides: true,
-  initialSlide: 0,
-  slideToClickedSlide: true,
-});
-
-new Swiper('.swiper4', {
-  width: 1400,
-  loop: false,
-  slidesPerView: 'auto',
-  initialSlide: 0,
-  slideToClickedSlide: true,
+  new Swiper('.swiper4', {
+    width: 1400,
+    loop: false,
+    slidesPerView: 'auto',
+    initialSlide: 0,
+    slideToClickedSlide: true,
+  });
 });
